4b7cd52144cc18b04b934ad9620e844f2a292d5c
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-01-31 20:59:48 -0500

    Added the review schema.

--- models/chars.js
+++ models/chars.js
@@ -3,10 +3,16 @@ require('mongoose-type-url');
 
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+    rating: { type: Number },
+    comment: { type: String, createdAt: new Date(0) }
+});
+
 const charSchema = new Schema({
     imageURL: { type: String, require, createdAt: new Date(0) },
     prompt: { type: String, require },
-    size: { type: String, require }
+    size: { type: String, require },
+    reviews: [reviewSchema]
 });
 
 module.exports = mongoose.model('Char', charSchema);
