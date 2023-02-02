86cef0dc0aa02cb04799226ba5937d659d330089
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-02-01 21:20:02 -0500

    Updated the user schema.

--- config/passport.js
+++ config/passport.js
@@ -8,6 +8,9 @@ passport.use(
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_SECRET,
             callbackURL: process.env.GOOGLE_CALLBACK
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            
         }
     )
 )
