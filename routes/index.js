76955309bb383f4f685dc345676c433c7ad3f724
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-02-01 23:56:03 -0500

    Added the routing code for passport.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const charsCtrl = require('../controllers/chars');
+const passport = require('passport');
 
-// Home page.
+// Home page and log in.
 router.get('/', charsCtrl.index);
+router.get('/auth/google', passport.authenticate('google',
+    {
+        scope: ['profile', 'email']
+    }
+));
+
+router.get('/oauth2callback', passport.authenticate('google',
+{
+    successRedirect: '/',
+    failureRedirect: '/',
+}));
 
 module.exports = router;
