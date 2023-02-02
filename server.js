e8b06092e63f29fe25df4ce2ea48014c10658709
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-02-01 20:51:21 -0500

    Added some parts of passport and cookie.

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: true
+    })
+);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
