dfdd9570adaf009a57412f7ae4917c44425bd9df
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-02-04 12:44:03 -0500

    Added the user schema to the review and char schemas.

--- models/chars.js
+++ models/chars.js
@@ -5,13 +5,15 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
     rating: { type: Number, min: 0, max: 10, required: true },
-    comment: { type: String, required: true }
+    comment: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
 const charSchema = new Schema({
     imageURL: { type: String, require},
     prompt: { type: String, require },
     reviews: [reviewSchema],
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     createdAt: { type: Date }
 });
 
